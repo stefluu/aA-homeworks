22f35d455148da12d794c449da2bc0d3addc304b
Stefanie Luu <luu.stefanie@gmail.com>
2018-09-20 01:10:49 -0700

    Complete Phase I

---
+++ W5D4/intro_to_JS.js
@@ -0,0 +1,38 @@
+// madLib
+
+const madLib = (verb, adj, noun) => {
+  "We shall $(verb) the $(adj) $(noun)."
+}
+
+// isSubstring
+const isSubstring = (searchString, subString) => {
+  ( searchString.indexOf(subString) !== 1 ) ? return true : return false;
+}
+
+
+// fizzBuzz
+
+const fizzBuzz = (array) => {
+  for(let i = 0; i < array.length; i++){
+    if (array[i] % 15 == 0) {
+      console.log("fizzbuzz");
+    } else if (array[i] % 3 == 0) {
+      console.log("fizz");
+    } else if (array[i] % 5 == 0) {
+      console.log("buzz");
+    } else {
+      console.log(array[i])
+    }
+  }
+}
+
+// isPrime
+
+const isPrime = (number) => {
+  for (let i = 2; i < number; i++){
+    if (number % i == 0){
+      return false
+    }
+  }
+  return true
+}
