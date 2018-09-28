54fffc697c4508c4314ccf93b3fa1f39376f9103
Stefanie Luu <luu.stefanie@gmail.com>
2018-09-28 01:56:16 -0700

    Setup Webpack

---
+++ W6D5/skeleton/webpack.config.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+  context: __dirname,
+  entry: "./index.jsx",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+}
